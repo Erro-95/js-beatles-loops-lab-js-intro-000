c1aa9ce0b61f7103c7b906f783ade1b73e6b3a29
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-30 01:27:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function theBeatlesPlay(musicians, instruments){
   let output = [];
 
   for(let i = 0 ; i < musicians.length ; i++){
-    output.push(`${musicians[i]} plays ${instruments[i]} `)
+    output.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
-  return output
+  return output;
 }
